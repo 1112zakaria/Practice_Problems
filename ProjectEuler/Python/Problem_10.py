a69fc04d55a1a0a6b379fa73a31016643a92c6be
Zakaria Ismail <zisma3@ocdsb.ca>
2020-09-16 09:46:28 -0400

    idk

--- ProjectEuler/Python/Problem_10.py
+++ ProjectEuler/Python/Problem_10.py
@@ -6,40 +6,35 @@ Find the sum of all the primes below two million.
 
 INCOMPLETE. TOO SLOW. INEFFICIENT.
 """
+import math
 
 
-def sum_of_primes(num: int) -> int:
+def sum_of_primes(n: int) -> int:
     """
-    RETURNS the sum of primes
-    below num
-
-    num is an integer
+    RETURNS the sum of primes below n.
 
     >>> sum_of_primes(10)
     17
     """
-    sum = 0
-    prime_list = []
-    for i in range(2, num):
-        print(i)
-        is_prime = True
-
-        if len(prime_list) == 0:
-            for j in range(2, i):
-                if i % j == 0:
-                    is_prime = False
-                    break
-        else:
-            for j in prime_list:
-                if i % j == 0:
-                    is_prime = False
-                    break
-
-        if is_prime:
-            sum += i
-            prime_list += [i]
-    return sum
+    somme = 2
+    for i in range(3, n, 2):
+        if is_prime(i):
+            somme += i
+    return somme
 
 
-n = 2000000
+def is_prime(n: int) -> bool:
+    """
+    RETURNS True or False whether n is prime or not.
+
+    >>> is_prime(3)
+    True
+    """
+    for i in range(3, math.ceil(math.sqrt(n)), 1):
+        if n % i != 0:
+            return False
+    return True
+
+
+n = 10
 print("The sum of primes below {} is {}".format(n, sum_of_primes(n)))
